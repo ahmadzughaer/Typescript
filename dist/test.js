7eb3e1827d66a94c9901ed27cc2c5122b1574d81
ahmadzughaer <ahmadzughaer@gmail.com>
2022-07-28 10:51:03 +0300

    config file options

--- dist/test.js
+++ dist/test.js
@@ -1,3 +1,10 @@
 "use strict";
 console.log(Math.round(9.3));
+let check = true;
+function showDetails(name, salary) {
+    if (check) {
+        return `Hello ${name}, your salary is ${salary}`;
+    }
+    return "nothing ";
+}
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,2 +1,11 @@
 // test
 console.log(Math.round(9.3))
+
+let check = true;
+
+function showDetails(name, salary) : string {
+    if(check) {
+        return `Hello ${name}, your salary is ${salary}`
+    }
+    return "nothing "
+}
