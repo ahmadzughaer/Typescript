6766390c52bd5f4b8314927d680844886c24ad83
ahmadzughaer <ahmadzughaer@gmail.com>
2022-07-28 10:41:27 +0300

    arrays

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,13 @@ let age = 32;
 let hire = true;
 let all = 100;
 all = "test";
+function add(num1, num2) {
+    return num1 + num2;
+}
+console.log(add("test", 5));
+let array1 = [1, 2, 3, 4, 5];
+let array2 = ["a", "b", "c", "d"];
+let array3 = [1, 2, 3, 4, 5, "a", "b", "c"];
+let array4 = [1, 2, 3, 4, 5, "a", "b", "c", ["a", "f"]];
+let array5 = [1, 2, 3, 4, 5, "a", "b", "c", ["a", "f"], false];
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,15 @@ let all:any = 100;
 
 all = "test"
 
+function add(num1:string, num2: number | string) {
+    return num1 + num2;
+}
+console.log(add("test",5))
+
+let array1:number[] = [1,2,3,4,5]
+let array2: string[] = ["a", "b","c","d"]
+let array3: (string | number)[] = [1,2,3,4,5,"a","b","c"]
+
+let array4: (string | number | string[])[] = [1,2,3,4,5,"a","b","c", ["a","f"]]
+
+let array5 = [1,2,3,4,5,"a","b","c", ["a","f"], false]
